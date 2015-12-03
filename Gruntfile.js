352e4bb4f03d60f7519fd2cf92a7b70feb4f98fa
Zack Chase <zack@distelli.com>
2015-12-03 13:26:09 -0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+// Gruntfile.js
+
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    // all of our configuration will go here
+    // configure jshint to validate js files -----------------------------------
+     coffee: {
+           coffee_to_js: {
+             options: {
+               bare: true,
+               sourceMap: true
+             },
+             expand: true,
+             flatten: true,
+             cwd: "client",
+             src: ["**/*.coffee"],
+             dest: 'client',
+             ext: ".js"
+           }
+         }
+
+
+    });
+
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+};
